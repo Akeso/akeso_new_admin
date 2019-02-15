6ee62869b08ac205ebbd688c1edb982e365ee8fc
WJ <jsj_wj@163.com>
2019-02-15 12:15:20 +0800

    npm run build

---
+++ dist/static/js/9DEo.a80aeaff.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["9DEo"],{"9DEo":function(e,n,t){"use strict";t.r(n);var i=t("KHd+"),l=Object(i.a)({},function(){var e=this.$createElement;return(this._self._c||e)("h1",[this._v("智能儿童")])},[],!1,null,null,null);l.options.__file="intelligent.vue";n.default=l.exports}}]);
