9931d5c4b4f11cde2182965b466632d8be6b01cd
WJ <jsj_wj@163.com>
2019-04-02 13:36:28 +0800

    整体分析 over

--- src/views/intelligent_data/components/whole.vue
+++ src/views/intelligent_data/components/whole.vue
@@ -10,6 +10,7 @@
             :label="item.label"
             :value="item.key"/>
         </el-select>
+        <el-button class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">查询</el-button>
       </el-col>
     </el-row>
     <el-row>
@@ -28,19 +29,19 @@
         </tr>
         <tr>
           <td>智能/全部儿童占比</td>
-          <td>{{ wholeData.deviceChildrenPercent }}</td>
+          <td>{{ wholeData.deviceChildrenPercent }}%</td>
         </tr>
         <tr>
           <td>户外活动情况达标率</td>
-          <td>{{ wholeData.outTimePercent }}</td>
+          <td>{{ wholeData.outTimePercent }}%</td>
         </tr>
         <tr>
           <td>体育运动情况达标率</td>
-          <td>{{ wholeData.stepCountPercent }}</td>
+          <td>{{ wholeData.stepCountPercent }}%</td>
         </tr>
         <tr>
           <td>近视用眼不良未达标率</td>
-          <td>{{ wholeData.nearworkDayPercent }}</td>
+          <td>{{ wholeData.nearworkDayPercent }}%</td>
         </tr>
       </table>
     </el-row>
@@ -56,44 +57,44 @@
         <tr>
           <td rowspan="3">近视健康因子情况</td>
           <td>户外时间/天</td>
-          <td>58分钟</td>
+          <td>{{ wholeData.outTimeAvg }}分钟</td>
           <td>≥120分钟</td>
-          <td>上下</td>
+          <td/>
         </tr>
         <tr>
           <td>阳光摄入/天</td>
-          <td>21万lux</td>
+          <td>{{ wholeData.luxDayAvg }}lux</td>
           <td>≥30万lux</td>
-          <td>上下</td>
+          <td/>
         </tr>
         <tr>
           <td>运动步数/天</td>
-          <td>6000步</td>
+          <td>{{ wholeData.stepCountAvg }}步</td>
           <td>≥10000步</td>
-          <td>上下</td>
+          <td/>
         </tr>
         <tr>
           <td rowspan="3">近视危险因子情况</td>
-          <td>用眼时长/天</td>
-          <td>359分钟</td>
+          <td>近距离用眼时长/天</td>
+          <td>{{ wholeData.nearworkDayAvg }}分钟</td>
           <td>≤240分钟</td>
-          <td>上下</td>
+          <td/>
         </tr>
         <tr>
           <td>不良用眼姿态/天</td>
-          <td>100次</td>
+          <td>{{ wholeData.badPostureTimesAvg }}次</td>
           <td>≤90分钟</td>
-          <td>上下</td>
+          <td/>
         </tr>
         <tr>
           <td>颈椎与用眼负荷/天</td>
-          <td>1002D</td>
+          <td>{{ wholeData.nearworkBurdenDayAvg }}D</td>
           <td>≤750D</td>
-          <td>上下</td>
+          <td/>
         </tr>
         <tr>
           <td>初步提示</td>
-          <td colspan="4">啊啊啊啊啊啊</td>
+          <td colspan="4">若想达到近视防控效果最大化，请多去户外运动，沐浴光照，三项健康因子全部达到参考值；同时避免错误用眼习惯增加近视风险，将导致近视加深的近视危险因子下降到参考值以下。</td>
         </tr>
       </table>
     </el-row>
@@ -118,6 +119,10 @@ export default {
     this.getData()
   },
   methods: {
+    handleFilter: function() {
+      console.log('aaaaaaaaa')
+      this.getData()
+    },
     getData: function() {
       fetchWhole({ selectDate: this.selectDate }).then(response => {
         this.wholeData = response.data
