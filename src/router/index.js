1f41c48bb9f640421afd04e1f46a197a123ead99
WJ <jsj_wj@163.com>
2019-04-16 09:47:20 +0800

    隐藏 消息管理

--- src/router/index.js
+++ src/router/index.js
@@ -170,18 +170,18 @@ export const constantRouterMap = [
       }
     ]
   },
-  {
-    path: '/messages',
-    component: Layout,
-    children: [
-      {
-        path: 'messages',
-        name: 'Messages',
-        component: () => import('@/views/messages/messages'),
-        meta: { title: 'messages', icon: 'bug' }
-      }
-    ]
-  },
+  // {
+  //   path: '/messages',
+  //   component: Layout,
+  //   children: [
+  //     {
+  //       path: 'messages',
+  //       name: 'Messages',
+  //       component: () => import('@/views/messages/messages'),
+  //       meta: { title: 'messages', icon: 'bug' }
+  //     }
+  //   ]
+  // },
   {
     path: '/devices',
     component: Layout,
