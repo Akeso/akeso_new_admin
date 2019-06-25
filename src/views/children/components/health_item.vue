40e745ef005cc696bb9a1fd11c9e4c55239261e6
WJ <jsj_wj@163.com>
2019-06-25 18:14:45 +0800

    后台 日报重构

--- src/views/children/components/health_item.vue
+++ src/views/children/components/health_item.vue
@@ -1,14 +1,97 @@
 <template>
   <div>
-    <span style="font-size: 18px; font-weight: bold;">健康评分</span>
+    <span style="font-size: 18px; font-weight: bold;">{{ text.title }}</span>
     <div style="font-size: 14px; margin: 5px 0px;">
-      诺瞳眼健康评分（eye-health index），
-      是借助“物联网”的数据采集技术和智能硬件对户外活动
-      及近距离用眼等近视相关指标进行精准量化计算得出的数值。
-      每⽇同步数据后起始分数设定在100分，最高分为220分，
-      其中户外活动相关指标占100分（加分项），近距离⼯作
-      相关指标占100分（减分项），运动步数占20分（额外加分），
-      每次同步数据后根据指标达成条件进⾏增减。
+      <span>{{ text.des }}</span>
     </div>
+    <el-row>
+      <el-col :span="24">
+        <!--<ve-histogram :color="colorHistogram" :legend="legendHistogram" :x-axis="xAxisHistogram" :y-axis="yAxisHistogram" :series="seriesHistogram"/>-->
+        <ve-histogram :color="colorHistogram" :x-axis="xAxisHistogram" :y-axis="yAxisHistogram" :series="seriesHistogram"/>
+      </el-col>
+    </el-row>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: Object,
+      default: function() {
+        return {
+          title: '',
+          des: '',
+          unit: ''
+        }
+      }
+    },
+    hours: {
+      type: Array,
+      default: function() {
+        return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      }
+    }
+  },
+  data() {
+    this.chartSettings = {
+      labelMap: {
+        score: '分数'
+      }
+    }
+    return {
+      colorHistogram: ['#26c281', '#003366'],
+      xAxisHistogram: {
+        type: 'category',
+        show: true,
+        // boundaryGap: true,
+        data: [
+          '00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00',
+          '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00',
+          '20:00', '21:00', '22:00', '23:00'
+        ]
+      },
+      // legendHistogram: {
+      //   data: ['户外时长']
+      // },
+      yAxisHistogram: {
+        name: this.text.unit,
+        nameLocation: 'end',
+        nameTextStyle: {
+          align: 'right'
+        },
+        type: 'value'
+      },
+      seriesHistogram: [
+        {
+          name: this.text.title,
+          type: 'bar',
+          barWidth: '30%',
+          label: {
+            // normal: {
+            show: true,
+            formatter: '{c}',
+            position: 'top',
+            fontSize: 12,
+            fontWeight: 'bold',
+            color: '#000000'
+          },
+          itemStyle: {
+            color: '#26c281'
+          },
+          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        }
+      ]
+    }
+  },
+  watch: {
+    'hours': function(newVal, oldVal) {
+      this.seriesHistogram[0].data = newVal
+    }
+  },
+  created() {
+  },
+  methods: {
+  }
+}
+</script>
