8760dcb525b923bf9cc33d02bb61b4ae16e5685f
WJ <jsj_wj@163.com>
2019-06-25 15:16:35 +0800

    fix

--- src/views/children/components/daily_score.vue
+++ src/views/children/components/daily_score.vue
@@ -1,34 +1,31 @@
 <template>
   <el-card class="box-card">
-    <div slot="header" class="clearfix">
-      <span>健康评分</span>
-      <el-tooltip effect="light" placement="right">
-        <div slot="content">
-          诺瞳眼健康评分（eye-health index），<br>
-          是借助“物联网”的数据采集技术和智能硬件对户外活动<br>
-          及近距离用眼等近视相关指标进行精准量化计算得出的数值。<br>
-          每⽇同步数据后起始分数设定在100分，最高分为220分，<br>
-          其中户外活动相关指标占100分（加分项），近距离⼯作<br>
-          相关指标占100分（减分项），运动步数占20分（额外加分），<br>
-          每次同步数据后根据指标达成条件进⾏增减。</div>
-        <i class="el-icon-question"/>
-      </el-tooltip>
+    <el-row style="margin-bottom: 10px;">
+      <el-col>
+        <label for="">选择日期</label>
+        <el-date-picker
+          :editable="false"
+          :clearable="false"
+          v-model="selectDate"
+          type="date"
+          placeholder="选择日期"
+          value-format="yyyy-MM-dd"
+          @change="changeDate"/>
+      </el-col>
+    </el-row>
+    <hr>
+    <span style="font-size: 18px; font-weight: bold;">健康评分</span>
+    <div style="font-size: 14px; margin: 5px 0px;">
+      诺瞳眼健康评分（eye-health index），
+      是借助“物联网”的数据采集技术和智能硬件对户外活动
+      及近距离用眼等近视相关指标进行精准量化计算得出的数值。
+      每⽇同步数据后起始分数设定在100分，最高分为220分，
+      其中户外活动相关指标占100分（加分项），近距离⼯作
+      相关指标占100分（减分项），运动步数占20分（额外加分），
+      每次同步数据后根据指标达成条件进⾏增减。
     </div>
     <el-row>
       <el-col :span="24">
-        <el-row style="margin-bottom: 10px;">
-          <el-col>
-            <label for="">选择日期</label>
-            <el-date-picker
-              :editable="false"
-              :clearable="false"
-              v-model="selectDate"
-              type="date"
-              placeholder="选择日期"
-              value-format="yyyy-MM-dd"
-              @change="changeDate"/>
-          </el-col>
-        </el-row>
         <el-row>
           <el-col :span="5">
             <div style="text-align: center;">
---
+++ src/views/children/components/health_item.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <span style="font-size: 18px; font-weight: bold;">健康评分</span>
+    <div style="font-size: 14px; margin: 5px 0px;">
+      诺瞳眼健康评分（eye-health index），
+      是借助“物联网”的数据采集技术和智能硬件对户外活动
+      及近距离用眼等近视相关指标进行精准量化计算得出的数值。
+      每⽇同步数据后起始分数设定在100分，最高分为220分，
+      其中户外活动相关指标占100分（加分项），近距离⼯作
+      相关指标占100分（减分项），运动步数占20分（额外加分），
+      每次同步数据后根据指标达成条件进⾏增减。
+    </div>
+  </div>
+</template>
