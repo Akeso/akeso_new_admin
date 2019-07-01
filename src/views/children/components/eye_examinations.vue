8f57ca9f35e77b46828e1f84af11e7014ad2d557
WJ <jsj_wj@163.com>
2019-07-01 10:36:19 +0800

    fix

--- src/views/children/components/eye_examinations.vue
+++ src/views/children/components/eye_examinations.vue
@@ -1,32 +1,38 @@
 <template>
-  <el-table
-    :data="tableData"
-    stripe
-    style="width: 100%">
-    <el-table-column
-      prop="createFileTime"
-      label="建档时间"
-      min-width="180"/>
-    <el-table-column
-      prop="childName"
-      label="儿童名称"
-      min-width="180"/>
-    <el-table-column
-      prop="organizationTitle"
-      label="机构名称"
-      min-width="180"/>
-    <el-table-column
-      prop="belongTo"
-      label="归属"
-      min-width="180"/>
-    <el-table-column
-      label="操作"
-      min-width="180">
-      <template slot-scope="scope">
-        <el-button type="text" size="small">查看</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div>
+    <div v-if="shengji">
+      正在升级中，敬请期待哦！
+    </div>
+    <el-table
+      v-else
+      :data="tableData"
+      stripe
+      style="width: 100%">
+      <el-table-column
+        prop="createFileTime"
+        label="建档时间"
+        min-width="180"/>
+      <el-table-column
+        prop="childName"
+        label="儿童名称"
+        min-width="180"/>
+      <el-table-column
+        prop="organizationTitle"
+        label="机构名称"
+        min-width="180"/>
+      <el-table-column
+        prop="belongTo"
+        label="归属"
+        min-width="180"/>
+      <el-table-column
+        label="操作"
+        min-width="180">
+        <template slot-scope="scope">
+          <el-button type="text" size="small">查看</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
@@ -40,6 +46,7 @@ export default {
   },
   data() {
     return {
+      shengji: true,
       tableData: []
     }
   },
